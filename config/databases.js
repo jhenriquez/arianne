b918a09a4b5649afa9ed0121890629fe965a605f
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-06-07 00:08:33 -0400

    Added database configuration stub.

---
+++ config/databases.js
@@ -0,0 +1,11 @@
+var db = {
+	kingslanding : {
+		server: "192.168.7.80\\MSSQLBETA",
+		username: "",
+		password: ""
+	},
+	oberyn : "mongodb://localhost/dorne-development",
+	doran : "" // production database
+};
+
+module.exports = db;
