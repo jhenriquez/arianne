8d0c6ac821ceaba8817a4c249d538ca70394e03a
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-06-08 22:08:43 -0400

    added missing middleware and properly initialized.

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 "use strict";
 
+// declare general dependencies
 var express = require('express'),
 	morgan = require('morgan'),
 	goose = require('mongoose'),
 	body = require('body-parser'),
+	cookie = require('cookie-parser'),
+	session = require('express-session'),
+	flash = require('connect-flash'),
 	swig = require('swig'),
 	app = express();
 
-// Configure View Engine
+// declare more specifically configured dependencies.
+var passport = require('./config/passport'),
+	databases = require('./config/databases'),
+	routes = require('./routes'),
+	api = requrie('./routes/api');
 
+// configure View Engine
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 
-// Configure Static Content
+// configure Middleware
+app.use(morgan());
+app.use(body());
+app.use(cookie());
+app.use(flash());
+app.use(session({ secret : 'Velvet Secret They Fear' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// configure Static Content
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (rq, rs) {
-	rs.render('layout', {title : 'index'});
-});
+// initialize routes (html and api)
+routes();
+api();
 
+//  initialize mongoose data connection
+goose.connect(databases.oberyn);
+
+// initialize server socket
 app.listen(3000);
 console.log('Listening on 3000');
