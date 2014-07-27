fcabe1d25b1d487ed207fb384c6b936b2599ca50
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-07-27 18:36:25 -0400

    Now the server won't know or provide any user authentication information. Only serve a page.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app, passport) {
 	app.get('/', function (rq, rs) {
-		rs.render('home', { user: rq.user });
+		rs.render('home');
 	});
 
 	app.get('/:name', function (rq, rs) {
