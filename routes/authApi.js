2c85b8a0ad77ebf7f50c61d978e81c603fe6eb7a
unknown <jhenriquez@DR-Developer.racoindustries.com>
2014-08-01 18:12:32 -0400

    Made /Authenticate route,

--- routes/authApi.js
+++ routes/authApi.js
@@ -1,7 +1,27 @@
+var User = require('../models/user'),
+	jwt = require('jsonwebtoken');
+
 module.exports = function (app) {
 
 	app.post('/authenticate',  function (rq, rs) {
-		console.log(rq.body);
-		rs.json(rq.body);
+		User.findOne({ username: rq.body.username }, function (err, user) {
+			if(err)
+				return rs.json({ err : { message: 'Internal Error. Contact Adminsitrator.' }});
+
+			if (!user)
+				return rs.json({ err : { message: 'Username does not exists.' }});
+
+			if(!user.validPassword(rq.body.password))
+				return rs.json({ err : { message: 'Invalid Password.' }});
+
+			var profile = {
+				name:  user.name,
+				username: user.password
+			};
+
+			var token = jwt.sign(profile, 'I\'m King!', { expiresInMinutes : 30 });
+
+			return rs.json({ token : token });
+		});
 	});
 }
