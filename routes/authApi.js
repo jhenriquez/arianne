1dc4bafb0b49f305833ddd1abfb457899029ee69
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-03 20:10:31 -0400

    Added /api/me resource to retrieve current user information from current token.

--- routes/authApi.js
+++ routes/authApi.js
@@ -6,22 +6,32 @@ module.exports = function (app) {
 	app.post('/authenticate',  function (rq, rs) {
 		User.findOne({ username: rq.body.username }, function (err, user) {
 			if(err)
-				return rs.json({ err : { message: 'Internal Error. Contact Adminsitrator.' }});
+				return rs.json({ err : { message: 'Internal Error. Contact Adminsitrator.', inner: err } });
 
 			if (!user)
-				return rs.json({ err : { message: 'Username does not exists.' }});
+				return rs.json({ err : { message: 'Username does not exists.' } });
 
 			if(!user.validPassword(rq.body.password))
-				return rs.json({ err : { message: 'Invalid Password.' }});
+				return rs.json({ err : { message: 'Invalid Password.' } });
 
 			var profile = {
-				name:  user.name,
-				username: user.password
+				id: user.id,
+				username:  user.username,
+				email: user.email
 			};
 
 			var token = jwt.sign(profile, 'I\'m King!', { expiresInMinutes : 30 });
 
 			return rs.json({ token : token });
+		});
+	});
+
+	app.get('/api/me', function (rq, rs) {
+		var token = rq.get('Authorization').split(' ')[1]; 
+		jwt.verify(token, 'I\'m King!', {}, function (err, profile) {
+			if(err)
+				return rs.json({ err: { message: 'Internal Error. Contact Administrator.',  inner: err } });
+			rs.json({ user: profile });
 		});
 	});
 }
