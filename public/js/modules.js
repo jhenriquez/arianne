526b318729ac9ef73d854456d283f59ac7e65010
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-10-24 16:14:24 -0400

    Added AngularUI Dependency

--- public/js/modules.js
+++ public/js/modules.js
@@ -2,7 +2,7 @@
 
 angular.module('CustomDirectives', []);
 
-angular.module('ApplicationModule', ['ngResource', 'ngRoute', 'CustomDirectives', 'ui.bootstrap', 'TokenAuthentication'])
+angular.module('ApplicationModule', ['ngResource', 'ngRoute', 'CustomDirectives', 'ui.bootstrap', 'ui.router', 'TokenAuthentication'])
 	.config(function($routeProvider, $locationProvider, $httpProvider, $handleForbidden, $tokenInjector) {
 
 		// configure routes
@@ -17,7 +17,7 @@ angular.module('ApplicationModule', ['ngResource', 'ngRoute', 'CustomDirectives'
 			})
 			.when('/:installation/notfound', {
 				templateUrl: 'partials/installation-notfound.html',
-				controller: 'PartialErrorController'	
+				controller: 'PartialErrorController'
 			})
 			.when('/:installation/:imei', {
 				templateUrl: 'partials/unit-general.html',
